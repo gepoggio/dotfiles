8e4b1b203b3b8db6c6f2f8a8511d87e4fed67394
Gerardo Poggio <ge.poggio@gmail.com>
2016-02-01 21:31:59 +0000

    added ST3 sync to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,21 @@ function doIt() {
 	rsync --exclude "brew.sh" --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
+
+	ST3SRC="${HOME}/init/ST3/"
+	if [ -d "$ST3SRC" ]; then
+		if [ $(uname -s) = "Darwin" ]; then
+			ST3DST="${HOME}/Library/Application Support/Sublime Text 3/Packages/User/"
+		elif [ $(uname -s) = "Linux" ]; then
+			ST3DST="${HOME}/.config/sublime-text-3/Packages/User/"
+		fi
+		if [ -d "$ST3DST" ]; then
+			echo "Syncing Sublime Text 3 config"
+			echo "From: $ST3SRC"
+			echo "To: $ST3DST"
+			rsync -avh "${ST3SRC}" "${ST3DST}"
+		fi
+	fi
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
